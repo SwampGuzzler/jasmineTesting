7cef7546713fff9d5b1ef04e107f5e5532b2c166
Lucas Cotner <lc07@uw.edu>
2014-08-14 13:06:39 -0700

    Customized specs

---
+++ jasmine/spec/animalSpec.js
@@ -0,0 +1,17 @@
+describe("Animal", function() {
+	it("should have 6 legs if it's an insect", function() {
+		var insect = new Animal();
+		insect.kind = "insect";
+		expect(insect.numLegs()).toBe(6);
+	});
+	it("should have 8 legs if it's a spider", function() {
+		var spider = new Animal();
+		spider.kind = "spider";
+		expect(spider.numLegs()).toBe(8);
+	});
+	it("shouldn't know how many legs a millipede has", function() {
+		var millipede = new Animal();
+		millipede.kind = "millipede";
+		expect(millipede.numLegs()).toBeUndefined();
+	});
+});
---
+++ jasmine/src/Animal.js
@@ -0,0 +1,9 @@
+function Animal() {}
+
+Animal.prototype.numLegs = function() {
+	if (this.kind == "insect") {
+		return 6;
+	} else if (this.kind == "spider") {
+		return 8;
+	}
+};
